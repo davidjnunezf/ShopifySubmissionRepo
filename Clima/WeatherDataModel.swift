baef16ad48831f58a89ef8d2def0a3d69f05381b
David Nunez <43526645+davidjnunezf@users.noreply.github.com>
2019-12-29 12:46:59 -0500

    Update WeatherDataModel.swift

--- Clima/WeatherDataModel.swift
+++ Clima/WeatherDataModel.swift
@@ -1,16 +1,10 @@
-//
-//  WeatherDataModel.swift
-//  WeatherApp
-//
-//  Created by Angela Yu on 24/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
 class WeatherDataModel {
 
-    //Declare your model variables here
+   
     
     var temperature:Int = 0
     var condition:Int = 0
